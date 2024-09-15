a76a2cb71a1ecbfc87982e88c0909fc00ab3012e
Muhammad-Saaad <javed.iqbal12458@gmail.com>
2024-09-15 08:11:18 +0500

    learning commit

--- 4_rag/2b_rag_basics_metadata.py
+++ 4_rag/2b_rag_basics_metadata.py
@@ -1,7 +1,7 @@
 import os
 
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 # Define the persistent directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,7 +9,10 @@ db_dir = os.path.join(current_dir, "db")
 persistent_directory = os.path.join(db_dir, "chroma_db_with_metadata")
 
 # Define the embedding model
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+# embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/text-embedding-004" , google_api_key='AIzaSyA77gUQw_Fzk2L4hJx_6fzQOSZipJn_ZTg'
+        )
 
 # Load the existing vector store with the embedding function
 db = Chroma(persist_directory=persistent_directory,
@@ -20,9 +23,8 @@ query = "How did Juliet die?"
 
 # Retrieve relevant documents based on the query
 retriever = db.as_retriever(
-    search_type="similarity_score_threshold",
-    search_kwargs={"k": 3, "score_threshold": 0.1},
-)
+                            search_type="similarity_score_threshold",
+                            search_kwargs={"k": 3, "score_threshold": 0.1},)
 relevant_docs = retriever.invoke(query)
 
 # Display the relevant results with metadata
